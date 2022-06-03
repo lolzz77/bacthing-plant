311cee9a4fa0885781cc7cf7243ada9a85f9f3d5
Preston Lo <prestonloz97@gmail.com>
2022-06-03 19:44:51 +0000

    sleep

--- core/mqtt.py
+++ core/mqtt.py
@@ -44,28 +44,27 @@ def connect_mqtt():
     return client
 
 # Subcribe to topic (e.g: Cement, Water, etc)
-def subscribe(client: mqtt_client, grade):
+def subscribeTopic(client: mqtt_client, topic, grade, key):
     # On_message = the func will only be triggered once server side sends msg
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        if(msg.topic == cementWeight):
-            cement = float(msg.payload.decode())
-            if (cement >= grade['cement']):
-                publish(client, cementTopic)
-        if(msg.topic == waterWeight):
-            water = float(msg.payload.decode())
-            if (water >= grade['water']):
-                publish(client, waterTopic)
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")    
+        amount = -1
+        if(msg.payload.decode().isdigit()):
+            amount = float(msg.payload.decode())
+        if (amount >= grade[key] or amount != -1):
+            publish(client, key)
         # _20mm = grade['20mm']
         # _10mm = grade['10mm']
         # sand = grade['sand']
         # d100 = grade['d100']
         # d45 = grade['d45']
 
-    client.subscribe(cementRelay)
-    client.subscribe(waterRelay)
-    client.subscribe(cementWeight)
-    client.subscribe(waterWeight)
+    # Subscribe to relay so you can control relay
+    client.subscribe(topic[0])
+
+    # Subcribe to weight, so you can see how much weight issued
+    client.subscribe(topic[1])
+
     client.on_message = on_message
 
 # To stop node red cement weight
@@ -86,22 +85,30 @@ def publish(client, topic):
     else:
         print(f"Failed to send message to topic {topic}")
 
+def getList(dict):
+    return dict.keys()
+
+def createThread(grade_list, client, grade_dict):
+    for x in grade_list:
+        topic = [x + '_relay', x + 'weight']
+        x = threading.Thread(target=subscribeTopic(client, topic ,grade_dict, x), name=x+'_t')
+        x.start()
 
 def run():
     client = connect_mqtt()
-    grade = None
-    while grade == None:
+    grade_dict = None
+    while grade_dict == None:
         print(gradePrint)
         inp_grade = input('Choose Grade:')
         match inp_grade:
             case '1':
-                grade = grade30NormalMix
+                grade_dict = grade30NormalMix
                 msg = 'Grade 30 Normal Mix'
             case '2':
-                grade = grade20NormalMix
+                grade_dict = grade20NormalMix
                 msg = 'Grade 20 Normal Mix'
             case '3':
-                grade = gradeCMotar
+                grade_dict = gradeCMotar
                 msg = 'Grade C Motar'
             # 'case _:' is the 'default:' in C++
             case _:
@@ -109,12 +116,13 @@ def run():
     print('Chosen grade: ' + msg)
 
     inp_meter = float(input('Enter Cubic Meter:'))
-    for x in grade:
-        grade[x] *= inp_meter
-    print(grade)
+    for x in grade_dict:
+        grade_dict[x] *= inp_meter
+    print(grade_dict)
 
-    
-    subscribe(client, grade)
+    grade_list = getList(grade_dict)
+    createThread(grade_list, client, grade_dict)
+
     # Paho Python client provides three loops
     # loop_start()
     # loop_forever()
@@ -135,5 +143,3 @@ def run():
     # run()
 
 run()
-
-# hello world
